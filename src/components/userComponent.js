ad4c640474ec208e64096d41d95740244197f589
bashtag <btstsiy@hotmail.com>
2023-07-01 15:34:55 +0300

    user operations component

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,41 +1,32 @@
-import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
-import Customer from "../models/customer.js"
-import User from "../models/user.js"
+import AbstractUser from "../modules/abstractUser.js"
 import UserService from "../services/userService.js"
+import MongoLogger from "../crossCuttingConcerns/logging/mongoLogger.js"
+import Customer from "../modules/Customer.js"
 
-console.log("User component yüklendi")
+console.log("User Component is loaded")
 
 let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
 
-let user1 = new User(1,"Engin","Demiroğ","Ankara")
-let user2 = new User(2,"Baran","Gökçekli","Muğla")
+let user1 = new AbstractUser(1,"Engin","Demiroğ","Ankara")
+let user2 = new AbstractUser(2,"Baran","Gökçekli","Muğla")
 userService.add(user1)
 userService.add(user2)
 
-//console.log(userService.list())
-//console.log(userService.getById(2))
-
-
-
+// console.log(userService.listErrors())
+// console.log(userService.getById(2))
 
 let customer = {id:1, firstName:"Engin"}
 
 //prototyping
 customer.lastName = "Demiroğ"
 
-console.log(customer.lastName)
-
 console.log("--------------------------")
-userService.load()
-
 
 let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","fdgdfg");
-customerToAdd.type = "customer"
 
 userService.add(customerToAdd)
-console.log(userService.customers)
-console.log(userService.employees)
-console.log(userService.errors)
+console.log(userService.listCustomers())
+console.log(userService.listEmployees())
+console.log(userService.listErrors())
 console.log(userService.getCustomersSorted())
-//22.00 Dersteyiz
