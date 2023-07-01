0e2786149477d899e0d0d09cb534f4dd6cab726d
bashtag <btstsiy@hotmail.com>
2023-07-01 15:33:04 +0300

    learned code

--- src/functions.js
+++ src/functions.js
@@ -1,103 +1,62 @@
-function addToCart(quantity,productName="Elma") {
-    console.log("Sepete eklendi : ürün : " 
-    + productName + " adet : " + quantity)
+export function addToCart(productName) {
+	console.log("Sepete eklendi : " + productName)
 }
 
-//addToCart()
-addToCart(10)
-//addToCart(15)
-
-let sayHello = ()=>{
-    console.log("Hello World!")
+let	sayHello = () => {
+	console.log("Hello World!")
 }
 
-sayHello()
+sayHello();
 
-let sayHello2 = function () {
-    console.log("Hello World 2")
+let	sayHello2 = function (...params) { // parameter with rest operator must be the last parameter
+	console.log("Hello World 2! " + params)
 }
 
-sayHello2();
+sayHello2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-function addToCart2(productName, quantity, unitPrice) {
-    
+function	addToCart2(productName, quantity, unitPrice) {
+	console.log("Ürün : " + productName + " Adet : " + quantity + " Fiyat : " + unitPrice)
 }
 
-addToCart2("Elma",5,10)
-addToCart2("Armut", 2, 20)
-addToCart2("Limon",3,15)
+addToCart2("Elma", 5, 10);
+addToCart2("Armut", 2, 20);
+addToCart2("Limon", 3, 15);
 
-let product1 = {productName:"Elma", unitPrice:10, quantity:5}
+console.log("--------------------")
 
-function addToCart3(product) {
-    console.log("Ürün : "+product.productName)
-    console.log("Adet : "+product.quantity)
-    console.log("Fiyat : "+product.unitPrice)
+function	addToCart3(product) {
+	console.log("Ürün : " + product.productName + " Adet : " + product.quantity + " Fiyat : " + product.unitPrice)
 }
 
+let	product1 = {productName: "Elma", quantity: 5, unitPrice: 10};
 
-addToCart3(product1)
+addToCart3(product1);
 
-let product2 = {productName:"Elma", unitPrice:10, quantity:5}
-let product3 = {productName:"Elma", unitPrice:10, quantity:5}
-product2 = product3
-product2.productName = "KARPUZ"
-console.log(product3.productName)
+let	product2 = {productName: "Armut", quantity: 2, unitPrice: 20};
 
-let sayi1 = 10
-let sayi2 = 20
-sayi1 = sayi2
-sayi2 = 100
-console.log(sayi1)
+product2 = product1;
+product2.productName = "Karpuz";
+console.log(product1.productName);
 
-function addToCart4(products) {
-    console.log(products)
-}
+// Objects are reference types
 
-let products = [
-    {productName:"Elma", unitPrice:10, quantity:5},
-    {productName:"Armut", unitPrice:10, quantity:5},
-    {productName:"Karpuz", unitPrice:10, quantity:5}
-]
+let	num1 = 10;
+let	num2 = 20;
+num2 = num1;
+num1 = 30;
+console.log(num2);
 
-addToCart4(products)
+// Primitive types are value types
+// string, number, bigint, boolean, undefined, symbol, null
 
-function add(bisey,...numbers) { //rest
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        total = total + numbers[i]
-    }
-    console.log(total)
-    console.log(bisey)
-}
 
-add(20,30)
-//add(20,30, 40)
-//add(20,30, 40, 50)
+let	[{productName:newName, quantity, unitPrice}, [sehir]] = [
+	product1,
+	[
+		["Ankara"],
+		["Bursa"]
+	]
+];
 
-let numbers = [30,10,500,600,120]
-//console.log(...numbers)
-console.log(Math.max(...numbers))
-
-let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
-    {name:"İç Anadolu", population:"20M"},
-    {name:"Marmara", population:"30M"},
-    {name:"Karadeniz", population:"10M"},
-    [
-        ["Ankara","Konya"],
-        ["İstanbul","Bursa"],
-        ["Sinop","Trabzon"],
-    ]
-]
-
-// console.log(icAnadolu.name)
-// console.log(icAnadolu.population)
-console.log(icAnadoluSehirleri)
-
-let newProductName, newUnitPrice, newQuantity
-({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
-= {productName:"Elma", unitPrice:10, quantity:5})
-
-console.log(newProductName)
-console.log(newUnitPrice)
-console.log(newQuantity)
+console.log(newName)
+console.log(sehir)
